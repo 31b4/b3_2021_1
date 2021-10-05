fef8fc0b12292929bbbe30b73330e1284821382b
31b4 <75566095+31b4@users.noreply.github.com>
2021-10-05 22:14:09 +0200

    Add files via upload

--- FAScript.js
+++ FAScript.js
@@ -1,19 +1,19 @@
 console.log("SZIA");
-data = '[{"name": "Jégvarázs","date": "2013","type":["vigjatek","animation","drama"]},{"name": "Másnaposok","date": "2009","type":["vigjatek"]}]';
+filmek='[{"name":"Jégvarázs","date":"2013","type":["vigjatek","animation","drama"]},{"name":"Másnaposok","date":"2009","type":["vigjatek"]},{"name":"Verdák","date":"2006","type":["vigjatek","animation"]},{"name":"Démonokközött","date":"2013","type":["horror"]},{"name":"Visszaajövőbe","date":"1985","type":["vigjatek","sci-fi"]},{"name":"Éhezőkviadala","date":"2012","type":["sci-fi","akcio","drama"]},{"name":"Miután","date":"2019","type":["romantika","drama","sex,softporn"]},{"name":"Pókember:Idegenben","date":"2019","type":["akcio","sci-fi","vigjatek"]}]'
+var mydata = JSON.parse(filmek);
 
-            
-/*
-var mydata = JSON.parse(data);
-console.log(mydata[0].name);
-console.log(mydata[0].date);
-console.log(mydata[0].type);
-*/
-/*
+for (let i = 0; i < filmek.length; i++) {
+    if (mydata[i]==null) {break;}
+    console.log(mydata[i].name);
+    console.log(mydata[i].date);
+    console.log(mydata[i].type);
+}            
+
 var tipus = "drama";
 var a = new Array();
 
 for (let i = 0; i < 10; i++) {
-    for (let k = 0; k < 3; k++) {
+    for (var k = 0; k < 3; k++) {
         if (data[i].type[k] == tipus) {
             a.push(data[i])
             console.log("ha ha ")
@@ -26,4 +26,4 @@ for (let i = 0; i < 10; i++) {
 for (let j = 0; j < a.length; j++) {
     console.log(a[i].name);
     
-}*/
+}
