2eb210ca978cda76f183aee615eb1da5e424f3a9
31b4 <75566095+31b4@users.noreply.github.com>
2021-10-06 19:39:27 +0200

    Add files via upload

--- FAScript.js
+++ FAScript.js
@@ -1,29 +1,100 @@
-console.log("SZIA");
-filmek='[{"name":"Jégvarázs","date":"2013","type":["vigjatek","animation","drama"]},{"name":"Másnaposok","date":"2009","type":["vigjatek"]},{"name":"Verdák","date":"2006","type":["vigjatek","animation"]},{"name":"Démonokközött","date":"2013","type":["horror"]},{"name":"Visszaajövőbe","date":"1985","type":["vigjatek","sci-fi"]},{"name":"Éhezőkviadala","date":"2012","type":["sci-fi","akcio","drama"]},{"name":"Miután","date":"2019","type":["romantika","drama","sex,softporn"]},{"name":"Pókember:Idegenben","date":"2019","type":["akcio","sci-fi","vigjatek"]}]'
+filmek='[{"name":"Jégvarázs","date":"2013","type":["vigjatek","animacio","drama"]},{"name":"Másnaposok","date":"2009","type":["vigjatek"]},{"name":"Verdák","date":"2006","type":["vigjatek","animacio"]},{"name":"Démonokközött","date":"2013","type":["horror"]},{"name":"Visszaajövőbe","date":"1985","type":["vigjatek","sci-fi"]},{"name":"Éhezőkviadala","date":"2012","type":["sci-fi","akcio","drama"]},{"name":"Miután","date":"2019","type":["romantika","drama","sex,softporn"]},{"name":"Pókember:Idegenben","date":"2019","type":["akcio","sci-fi","vigjatek"]}]'
 var mydata = JSON.parse(filmek);
 
-for (let i = 0; i < filmek.length; i++) {
-    if (mydata[i]==null) {break;}
-    console.log(mydata[i].name);
-    console.log(mydata[i].date);
-    console.log(mydata[i].type);
-}            
-
-var tipus = "drama";
-var a = new Array();
-
-for (let i = 0; i < 10; i++) {
-    for (var k = 0; k < 3; k++) {
-        if (data[i].type[k] == tipus) {
-            a.push(data[i])
-            console.log("ha ha ")
-        }   
-        
-        console.log("Becsüld meg ami van")
-        
+function TipusKereses(id){
+    let kivantTipusSV = new Array();
+    for (let i = 0; i < 7; i++) {
+        let checkBox = document.getElementById(id+i);
+        if (checkBox.checked==true) {
+            kivantTipusSV.push(checkBox.value)
+        }
+    }
+    return kivantTipusSV;
+}
+function CheckRemove() {
+    for (let i = 0; i < 7; i++) {
+        let checkBox1 = document.getElementById("id"+i);
+        let checkBox2 = document.getElementById("noid"+i);
+        checkBox1.checked = false;
+        checkBox2.checked = false;
     }
 }
-for (let j = 0; j < a.length; j++) {
-    console.log(a[i].name);
-    
+function AzonosType(kivantTipus,nemKivantTipus){
+    for (let i = 0; i < kivantTipus.length; i++) {
+        for (let j = i; j < nemKivantTipus.length; j++) {
+            if (kivantTipus[i]==nemKivantTipus[j]) {
+                alert("Ne legyen azonos a kívánt és nem kívánt típusok között :)");
+                CheckRemove();
+            }
+        }
+    }
+}
+function FilmekKizarasa(mydata,kivanatosTipus, nemKivantTipus) {
+    var lehetHogyJo = new Array();
+    for (let i = 0; i < mydata.length; i++) {
+        var kilep = false;
+        for (let j = 0; j < mydata[i].type.length; j++) {
+            kilep = false;
+            for (let k = 0; k < nemKivantTipus.length; k++) {
+                if (mydata[i].type[j] == nemKivantTipus[k]) {
+                    kilep=true;
+                    break;
+                } 
+            }
+            if (kilep==true) {
+                break;
+            }
+        }
+        if (kilep == false) {
+            lehetHogyJo.push(mydata[i]);
+        }
+    }
+    //console.log(lehetHogyJo);
+    JoFilmek(kivanatosTipus,lehetHogyJo);
+}
+function JoFilmek(kivanatosTipus,lehetHogyJo) {
+    var ajanlottFilmek = new Array();
+    for (let i = 0; i < lehetHogyJo.length ; i++) {
+        for (let j = 0; j < kivanatosTipus.length; j++) {
+            var kilep = false;
+            for (let k = 0; k < lehetHogyJo[i].type.length; k++) {
+                if (kivanatosTipus[j] == lehetHogyJo[i].type[k]) {
+                    ajanlottFilmek.push(lehetHogyJo[i]);
+                    kilep = true;
+                    break;
+                }
+            }
+            if (kilep==true) {
+                break;
+            }
+        }
+    }
+    /*
+        for (let i = 0; i < ajanlottFilmek.length; i++) {
+            console.log("vegleges "+ajanlottFilmek[i].name);
+        }
+    */
+    KiIras(ajanlottFilmek);
+}
+function KiIras(ajanlottFilmek) {
+    var filmDiv = document.getElementById("ajanlottFilmek")
+    for (let i = 0; i < ajanlottFilmek.length; i++) {
+        var p = document.createElement("p");
+        p.style.fontSize="20px";
+        p.innerHTML=ajanlottFilmek[i].name;
+        filmDiv.appendChild(p);
+    }
+}
+function Kereses () {
+    document.getElementById("ajanlottFilmek").innerHTML="";
+    var kivantTipus = new Array();
+    var nemKivantTipus = new Array();
+    kivantTipus = TipusKereses("id");
+    nemKivantTipus = TipusKereses("noid");
+    /*
+    console.log("kivant:::::"+kivantTipus);
+    console.log("NEMkivant:::::"+nemKivantTipus);
+    */
+    AzonosType(kivantTipus,nemKivantTipus);
+    FilmekKizarasa(mydata,kivantTipus,nemKivantTipus);
 }
